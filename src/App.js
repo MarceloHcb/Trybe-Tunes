7544580cd20ee9d0c88ce7dbe458fae5058a644f
MarceloHcb <marcelohcb@msn.com>
2022-12-13 15:51:50 -0300

    fix requisito 1

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
           <Route exact path="/" component={ Login } />
           <Route exact path="/search" component={ Search } />
           <Route exact path="/album/:id" component={ Album } />
-          <Route exact path="/facorites" component={ Favorites } />
+          <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
           <Route path="*" component={ NotFound } />
